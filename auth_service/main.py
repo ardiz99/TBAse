747e3be9dadfa82f8b28edcf572b0b7cec88dbe1
Felice644 <felice.tortorelli24@gmail.com>
2024-11-14 10:20:37 +0100

    fix problemi con l'interfaccia di swagger

--- auth_service/main.py
+++ auth_service/main.py
@@ -3,26 +3,31 @@ from authlib.jose import JsonWebToken, JoseError
 import mysql.connector
 import os
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_cors import CORS 
 
 app = Flask(__name__)
+CORS(app)  # Abilita CORS per l'intera app consentendo la gestione del server Flask per consentire richieste da altre origini.
 
-# Configure JWT with Authlib
+
+# Configura JWT con Authlib
 jwt = JsonWebToken(['HS256'])
-SECRET_KEY = "your-secret-key"  # This should be stored securely
+SECRET_KEY = "your-secret-key"  # Questo valore deve essere mantenuto sicuro
 
-# Database connection function
+# Funzione di connessione al database
 def get_db_connection():
     return mysql.connector.connect(
         host=os.getenv("DATABASE_HOST", "localhost"),
-        user=os.getenv("DATABASE_USER", "root"),
-        password=os.getenv("DATABASE_PASSWORD", "password"),
+        user=os.getenv("DATABASE_USER", "gachauser"),
+        password=os.getenv("DATABASE_PASSWORD", "123456"),
         database=os.getenv("DATABASE_NAME", "ase")
     )
 
-# User registration route
+
+# Endpoint per la registrazione
 @app.route('/register', methods=['POST'])
 def register():
     data = request.json
+    user_id = data.get('UserId')
     first_name = data.get('FirstName')
     last_name = data.get('LastName')
     email = data.get('Email')
@@ -35,16 +40,16 @@ def register():
     cursor = conn.cursor()
     cursor.execute(
         """
-        INSERT INTO users ( FirstName, LastName, Email, Password, CurrencyAmount)
-        VALUES (%s, %s, %s, %s, %s)
+        INSERT INTO users (UserId, FirstName, LastName, Email, Password, CurrencyAmount)
+        VALUES (%s, %s, %s, %s, %s, %s)
         """,
-        (first_name, last_name, email, hashed_password, currency_amount)
+        (user_id, first_name, last_name, email, hashed_password, currency_amount)
     )
     conn.commit()
     conn.close()
     return jsonify(message="User registered"), 201
 
-# User login route
+# Endpoint per il login
 @app.route('/login', methods=['POST'])
 def login():
     email = request.json.get('Email')
@@ -57,7 +62,7 @@ def login():
     conn.close()
 
     if user and check_password_hash(user[0], password):
-        # Create JWT token using Authlib
+        # Crea il token JWT
         header = {'alg': 'HS256'}
         payload = {'sub': email}
         token = jwt.encode(header, payload, SECRET_KEY)
@@ -65,7 +70,7 @@ def login():
     else:
         return jsonify(error="Invalid credentials"), 401
 
-# Protected endpoint example
+# Endpoint di accesso protetto
 @app.route('/protected', methods=['GET'])
 def protected():
     token = request.headers.get('Authorization').split()[1]
